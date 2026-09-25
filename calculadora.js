c10eb2db9bbad95d7d3cbd626cd62e2b191772b2
MarioOcanaVilchez <marioocana1107@gmail.com>
2026-09-25 20:19:41 +0200

    Arreglo bug poner texto

--- calculadora.js
+++ calculadora.js
@@ -143,6 +143,7 @@ function cambiaOperacionTeclado(){
         caracter = input.substring(input.length - 1);
         if (numeros.includes(caracter)) aniadeNum(document.getElementById('num').textContent,document.getElementById('num2').textContent,document.getElementById('signo').textContent,input.substring(input.length - 1)); //añade numero
         else if (operaciones.includes(caracter)) aniadeSigno(input.substring(input.length - 1),document.getElementById('num').textContent,document.getElementById('num2').textContent,document.getElementById('signo').textContent);//añade operacion
+        else input.value = textoAnt;
         //Quitar valor
     } else {
         if (textoAnt == '') return;
